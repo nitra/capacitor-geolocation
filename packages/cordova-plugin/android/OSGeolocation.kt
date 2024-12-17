32f9b5b0ae6a67aeee273364e96aca361f3b90d4
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2024-12-17 17:25:31 +0000

    chore: remove unused import

--- packages/cordova-plugin/android/OSGeolocation.kt
+++ packages/cordova-plugin/android/OSGeolocation.kt
@@ -14,7 +14,6 @@ import com.google.gson.Gson
 import com.outsystems.plugins.osgeolocation.model.OSLocationOptions
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.launch
 
 /**
