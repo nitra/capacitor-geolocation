913c0f018d8f97f414b637c46c7baff89d28ef77
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2024-12-17 10:37:43 +0000

    chore: add dist to repo
    References: https://outsystemsrd.atlassian.net/browse/RMET-3904

---
+++ packages/cordova-plugin/dist/defaults.d.ts
@@ -0,0 +1,4 @@
+import { ClearWatchOptions, PositionOptions } from './definitions';
+
+export declare const PositionOptionsDefault: PositionOptions;
+export declare const ClearWatchOptionsDefault: ClearWatchOptions;
