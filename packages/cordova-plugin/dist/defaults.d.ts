45cf1dac46079bc761b794d631333cf8a51474f6
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2024-12-17 10:35:02 +0000

    Revert "chore: generate dist"
    This reverts commit 588f5658dea9720e71e7498559993f558be5533e.

--- packages/cordova-plugin/dist/defaults.d.ts
+++
@@ -1,4 +0,0 @@
-import { ClearWatchOptions, PositionOptions } from './definitions';
-
-export declare const PositionOptionsDefault: PositionOptions;
-export declare const ClearWatchOptionsDefault: ClearWatchOptions;
