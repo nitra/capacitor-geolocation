e1ecd4333b32dde5331572dd11bebe1f66bd8467
Marta Carlos <marta.carlos@outsystems.com>
2024-12-17 09:41:46 +0000

    chore: initial iOS-lib skeleton
    defines the initial skeleton

---
+++ packages/ios-lib/OSGeolocationLibTests/OSGLOCManagerTests.swift
@@ -0,0 +1,10 @@
+import OSGeolocationLib
+import XCTest
+
+final class OSGLOCManagerTests: XCTestCase {
+    
+}
+
+extension OSGLOCManagerTests {
+    func makeSUT() -> OSGLOCManager{  .init(locManager: .init())}
+}
